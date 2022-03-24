3afea8912a6a776726f4013dd93e738629b68040
YeshayahuBennun <ybennun@yahoo.com>
2022-03-24 20:38:19 +0200

    commit#2 - LocalizationT App -  Setting Up FusedLocationProvider

--- app/src/main/java/com/ybennun/location/MainActivity.java
+++ app/src/main/java/com/ybennun/location/MainActivity.java
@@ -1,14 +1,24 @@
 package com.ybennun.location;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.app.SearchManager;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -24,10 +34,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private GoogleApiClient client;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissions = new ArrayList<>();
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +54,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        //lets add permission to we need to request location of the users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -49,6 +80,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private ArrayList<String> permissionsToRequest(ArrayList<String> wantedPermissions) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for(String perm:wantedPermissions){
+            if(!hasPermission(perm)){
+                result.add(perm);
+            }
+        }
+
+        return null;
+    }
+
+    private boolean hasPermission(String perm) {
+        return checkCallingOrSelfPermission(perm)== PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
@@ -92,10 +139,30 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "No services", Toast.LENGTH_LONG).show();
                     });
             assert errorDialog != null;
-                errorDialog.show();
+            errorDialog.show();
             finish();
-        }else{
+        } else {
             Toast.makeText(MainActivity.this, "All is good!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
